d43759ca34238e6af6336ab7d54d6cc92a6faf50
Sabir <93681489+sabircodes@users.noreply.github.com>
2024-03-02 03:00:18 +0530

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -26,7 +26,7 @@ function Homepage() {
     <>
       <Title level={2} className="heading" >Global Crypto Stats</Title> 
       <Row>
-{/*         <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total}/></Col> */}
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total}/></Col>
         <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
         <Col span={12}><Statistic title="Total market Cap" value={millify(globalStats.totalMarketCap)}/></Col>
         <Col span={12}><Statistic title="Total 24h volume" value={millify(globalStats.total24hVolume)}/></Col>
